7901f8cfd1e29bcf3965dd2f3a4c77dabb9451b1
Adrien Stefanski <KnutFr@users.noreply.github.com>
2024-03-12 18:22:53 +0100

    chore(step4): step4 done

--- first-iac/main.tf
+++ first-iac/main.tf
@@ -8,3 +8,11 @@ resource "local_file" "this" {
   content  = var.file_content
   filename = "${path.module}/test.txt"
 }
+
+module "spells" {
+  source = "./modules/spells"
+
+  level_eldritch = 4
+  level_fire = 5
+  level_frost = 2
+}
---
+++ first-iac/modules/spells/variables.tf
@@ -0,0 +1,18 @@
+variable "level_frost" {
+    default = 0
+    type = number
+    description = "The level of the frost spell"
+}
+
+
+variable "level_fire" {
+    default = 0
+    type = number
+    description = "The level of the fire spell"
+}
+
+variable "level_eldritch" {
+    default = 0
+    type = number
+    description = "The level of the eldritch spell"
+}
